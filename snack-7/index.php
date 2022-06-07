f34232694e38fe7190ed9af4a71f8697ec4566bd
Guidocode <guidobuono@icloud.com>
2022-06-07 17:57:38 +0200

    Snack 7

---
+++ snack-7/index.php
@@ -0,0 +1,59 @@
+<!-- 
+  ## Snack 7
+  Creare un array contenente qualche alunno di un’ipotetica classe. 
+  Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. 
+  Stampare Nome, Cognome e la media dei voti di ogni alunno.
+-->
+
+<?php
+
+  $alumns = [
+    [
+      'name' => 'Ugo',
+      'lastname' => 'De Ughi',
+      'grades' => [9, 6, 10, 7, 8, 5]
+    ],
+    [
+      'name' => 'Mario',
+      'lastname' => 'Rossi',
+      'grades' => [9, 4, 8, 6, 8, 6]
+    ],
+    [
+      'name' => 'Gianni',
+      'lastname' => 'Neri',
+      'grades' => [9, 3, 10, 7, 6, 4]
+    ],
+    [
+      'name' => 'Giuseppe',
+      'lastname' => 'Verdi',
+      'grades' => [9, 6, 7, 7, 4, 3]
+    ]
+  ];
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Snack 7</title>
+</head>
+<body>
+
+  <h2>Classe 61 - Media voti</h2>
+
+  <?php for ($i=0; $i < count($alumns); $i++): ?>
+    
+    <p> <?php echo $alumns[$i]["name"] . ' ' . '-' . ' '  . $alumns[$i]["lastname"] ?> </p>
+
+    <span>Media voti: <?php echo $average = round( array_sum($alumns[$i]['grades']) / count($alumns[$i]['grades']), 1 ); ?> </span>
+
+    <hr>
+
+  <?php endfor ?>
+ 
+</body>
+</html>
